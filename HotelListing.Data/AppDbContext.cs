01b3e6bb4771668a7584a821cad78b0bd6971ac2
Uzor Nwachukwu <uzor1997@gmail.com>
2022-06-11 21:40:21 +0100

    initial commit

---
+++ HotelListing.Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using HotelListing.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+    }
+}
